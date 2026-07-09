957cb88e277242cd26e7c2dd5a52da540854ba35
ToUyen69 <banhtrangnuong20k@gmail.com>
2026-07-09 14:22:24 +0700

    fix: load images from static assets instead of backend

--- my-app/src/app/img-url.pipe.ts
+++ my-app/src/app/img-url.pipe.ts
@@ -1,5 +1,4 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { environment } from '../environments/environment';
 
 @Pipe({ name: 'imgUrl', standalone: true })
 export class ImgUrlPipe implements PipeTransform {
@@ -8,9 +7,6 @@ export class ImgUrlPipe implements PipeTransform {
     if (value.startsWith('http://') || value.startsWith('https://') || value.startsWith('assets/')) {
       return value;
     }
-    if (value.startsWith('uploads/')) {
-      return `${environment.apiBase}/${value}`;
-    }
-    return `${environment.apiBase}/uploads/${value}`;
+    return value.replace(/^uploads\//, '');
   }
 }
